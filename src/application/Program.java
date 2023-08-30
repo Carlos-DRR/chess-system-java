6faff66995b0fbaafff24b50f1b16358c0125ec4
Carlos Dimitri <carlosdimitriem@outlook.com>
2023-08-30 17:59:52 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,13 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
-		System.out.println("Sim");
+		Position p = new Position(1, 2);
+		System.out.println(p);
 	}
 
 }
