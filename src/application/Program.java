583ce530b11e4c4dbaae16db34f7b83538a5f16b
Carlos Dimitri <carlosdimitriem@outlook.com>
2023-09-11 13:52:15 -0300

    Check mate logic

--- src/application/Program.java
+++ src/application/Program.java
@@ -17,7 +17,7 @@ public class Program {
 		Scanner sc = new Scanner(System.in);
 		ChessMatch cm = new ChessMatch();
 		List<ChessPiece> captured = new ArrayList<>();
-		while(true) {
+		while(!cm.getCheckMate()) {
 			try {
 				UI.clearScreen();
 				UI.printMatch(cm, captured);
@@ -44,7 +44,8 @@ public class Program {
 			}
 
 		}
-		
+		UI.clearScreen();
+		UI.printMatch(cm, captured);
 	}
 
 }
